311ad9f372e5d2e0dfea898f90198a2eb0f5c14f
= <ddtank.marcus@gmail.com>
2018-09-15 19:56:31 -0300

    Some Fixes

--- SRL/DNVM.cs
+++ SRL/DNVM.cs
@@ -5,17 +5,29 @@ using System.CodeDom.Compiler;
 using System.Globalization;
 using System.Linq;
 using System.Reflection;
-class DotNetVM {
+
+internal class DotNetVM {
     public enum Language {
         CSharp, VisualBasic
     }
-    internal DotNetVM(string Content, Language Lang = Language.CSharp) {
-        if (System.IO.File.Exists(Content)) {
-            DllInitialize(Content);
-            return;
-        }
+    internal DotNetVM(byte[] File) {
+        Engine = Assembly.Load(File);
+    }
 
-        System.IO.StringReader Sr = new System.IO.StringReader(Content);
+    internal DotNetVM(string Code) {
+        System.IO.StringReader Sr = new System.IO.StringReader(Code);
+        string[] Lines = new string[0];
+        while (Sr.Peek() != -1) {
+            string[] tmp = new string[Lines.Length + 1];
+            Lines.CopyTo(tmp, 0);
+            tmp[Lines.Length] = Sr.ReadLine();
+            Lines = tmp;
+        }
+        Engine = InitializeEngine(Lines, Language.CSharp);
+    }
+
+    internal DotNetVM(string Code, Language Lang) {
+        System.IO.StringReader Sr = new System.IO.StringReader(Code);
         string[] Lines = new string[0];
         while (Sr.Peek() != -1) {
             string[] tmp = new string[Lines.Length + 1];
@@ -26,13 +38,11 @@ class DotNetVM {
         Engine = InitializeEngine(Lines, Lang);
     }
 
-    private void DllInitialize(string Dll) {
-        Engine = Assembly.LoadFrom(Dll);
-    }
+
     Assembly Engine;
-    internal static void Crash() {
+
+    internal static void Crash() =>
         Crash();
-    }
 
     /// <summary>
     /// Call a Function with arguments and return the result
@@ -41,92 +51,59 @@ class DotNetVM {
     /// <param name="FunctionName">Target function name</param>
     /// <param name="Arguments">Function parameters</param>
     /// <returns></returns>
-    internal dynamic Call(string ClassName, string FunctionName, params object[] Arguments) {       
-        return exec(Arguments, ClassName, FunctionName, Engine);
+    internal dynamic Call(string ClassName, string FunctionName, params object[] Arguments) =>
+        Exec(Arguments, ClassName, FunctionName, Engine);
+
+
+    private object Instance = null;
+
+    internal void StartInstance(string Class, params object[] Arguments) {
+        Type fooType = Engine.GetType(Class);
+        Instance = Activator.CreateInstance(fooType, Arguments);
     }
 
-    private string LastClass;
-    private object Instance = null;
-    private object exec(object[] Args, string Class, string Function, Assembly assembly) {
-        if (LastClass != Class)
-            Instance = null;
-        LastClass = Class;
-
+    private object Exec(object[] Args, string Class, string Function, Assembly assembly) {
         Type fooType = assembly.GetType(Class);
+        if (Instance == null)
+            Instance = assembly.CreateInstance(Class);
+        MethodInfo[] Methods = fooType.GetMethods().Where(x => x.Name == Function && x.GetParameters().Length == Args.Length).Select(x => x).ToArray();
 
-        MethodInfo[] Methods = fooType.GetMethods().Where(x => x.Name == Function).Select(x => x).ToArray();
-
+        Exception Error = new Exception("Failed to search for the method.");
         foreach (MethodInfo Method in Methods) {
-            if (Method.GetParameters().Length == Args.Length) {
-                try {
-                    if (Instance == null && !Method.IsStatic)
-                        Instance = assembly.CreateInstance(Class);
-
-                    return Method?.Invoke(Instance, BindingFlags.InvokeMethod, null, Args, CultureInfo.CurrentCulture);
-                } catch (Exception ex) {
-                    if (Method == Methods.Last())
-                        throw ex;
-                }
+            try {
+                return Method?.Invoke(Instance, BindingFlags.InvokeMethod, null, Args, CultureInfo.CurrentCulture);
+            } catch (Exception ex) {
+                Error = ex;
             }
         }
 
-        throw new Exception("Failed to find the method...");
+        throw Error.InnerException;
     }
 
-    const string ImportFlag = "#import ";
     private Assembly InitializeEngine(string[] lines, Language Lang) {
-        CodeDomProvider cpd = (Lang == Language.CSharp ? new CSharpCodeProvider() : (CodeDomProvider)new VBCodeProvider());
-
+        CodeDomProvider cpd = (Lang == Language.CSharp ? (CodeDomProvider)new CSharpCodeProvider() : new VBCodeProvider());
         var cp = new CompilerParameters();
         string sourceCode = string.Empty;
-        int Imports = 0;
         foreach (string line in lines) {
-            if (line.ToLower().StartsWith(ImportFlag)) {
-                cp.ReferencedAssemblies.Add(line.Substring(ImportFlag.Length, line.Length - ImportFlag.Length).Trim());
-                Imports++;
+            if (line.StartsWith("#IMPORT ")) {
+                string dll = line.Substring(8, line.Length - 8).Replace("%CD%", AssemblyDirectory);
+                cp.ReferencedAssemblies.Add(dll);
                 continue;
             }
-            sourceCode += line + "\r\n";
+            sourceCode += line.Replace("\t", "") + '\n';
         }
         cp.GenerateExecutable = false;
         CompilerResults cr = cpd.CompileAssemblyFromSource(cp, sourceCode);
-
-        if (cr.Errors.HasErrors) {
-            string Log = "Interpreter Error(s) List:";
-            foreach (CompilerError Error in cr.Errors) {
-                string MSG = string.Format("[{0}]: {1}", Error.Line - Imports, Error.ErrorText);
-                Log += "\n" + MSG;
-            }
-
-            throw new Exception(Log);
-        }
-
         return cr.CompiledAssembly;
+
     }
 
-    /*internal string CreateExe(string Source, string MainClass) {
-        CodeDomProvider cpd = new CSharpCodeProvider();
-        var cp = new CompilerParameters();
-        cp.GenerateExecutable = true;
-        cp.GenerateInMemory = false;
-        cp.MainClass = MainClass;
-        cp.IncludeDebugInformation = true;
-        cp.OutputAssembly = MainClass;
-        string sourceCode = string.Empty;
-        System.IO.StringReader Sr = new System.IO.StringReader(Source);
-        string[] Lines = new string[0];
-        while (Sr.Peek() != -1) {
-            string[] tmp = new string[Lines.Length + 1];
-            Lines.CopyTo(tmp, 0);
-            tmp[Lines.Length] = Sr.ReadLine();
-            Lines = tmp;
+    public static string AssemblyDirectory {
+        get {
+            string codeBase = Assembly.GetExecutingAssembly().CodeBase;
+            UriBuilder uri = new UriBuilder(codeBase);
+            string path = Uri.UnescapeDataString(uri.Path);
+            return System.IO.Path.GetDirectoryName(path);
         }
-        foreach (string line in Lines) {
-            if (line.StartsWith("using ") && line.EndsWith(";"))
-                cp.ReferencedAssemblies.Add(line.Substring(6, line.Length - 7) + ".dll");
-            sourceCode += line.Replace("\t", "") + '\n';
-        }
-        CompilerResults cr = cpd.CompileAssemblyFromSource(cp, sourceCode);
-        return cr.PathToAssembly;
-    }*/
+    }
 }
--- SRLUnity/Main.cs
+++ SRLUnity/Main.cs
@@ -1,9 +1,31 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 namespace SRLUnity {
-    public static class Wrapper
-    {
+    public static class Wrapper {
+        public static string AssemblyDirectory {
+            get {
+                string codeBase = Assembly.GetExecutingAssembly().CodeBase;
+                UriBuilder uri = new UriBuilder(codeBase);
+                string path = Uri.UnescapeDataString(uri.Path);
+                return Path.GetDirectoryName(path);
+            }
+        }
+
+        public static string DataDirectory {
+            get {
+                return Path.GetDirectoryName(AssemblyDirectory);
+            }
+        }
+
+        public static string GameDirectory {
+            get {
+                return Path.GetDirectoryName(DataDirectory);
+            }
+        }
+
         static ProcessStd Function = null;
         public static bool Is64Bits = IntPtr.Size == 8;
 
@@ -30,10 +52,37 @@ namespace SRLUnity {
 
         private static void Initialize() {
             if (Function == null) {
-                IntPtr DLL = NativeMethods.LoadLibrary(Is64Bits ? "SRLx64.dll" : "SRLx86.dll");
-                IntPtr Func = NativeMethods.GetProcAddress(DLL, "ProcessStd");
-                Function = Marshal.GetDelegateForFunctionPointer(Func, typeof(ProcessStd)) as ProcessStd;
+                try {
+                    string DLLName = Is64Bits ? "SRLx64.dll" : "SRLx32.dll";
+                    string[] Paths = new string[] {
+                        GameDirectory + "/",
+                        DataDirectory + "/Managed/",
+                        DataDirectory + "/Plugins/",
+                    };
+
+                    int Loop = 0;
+                    IntPtr DLL = IntPtr.Zero;
+                    while (DLL == IntPtr.Zero) {
+                        if (!File.Exists(Paths[Loop] + DLLName)) {
+                            Loop++;
+                            continue;
+                        }
+
+                        DLL = NativeMethods.LoadLibrary(Paths[Loop] + DLLName);
+                    }
+                    IntPtr Func = NativeMethods.GetProcAddress(DLL, "ProcessStd");
+                    Function = Marshal.GetDelegateForFunctionPointer(Func, typeof(ProcessStd)) as ProcessStd;
+                } catch (Exception ex){
+                    Console.WriteLine(ex.ToString());
+                }
             }
+        }
+
+        private static T[] AppendArray<T>(T[] Array, T Value) {
+            T[] New = new T[Array.Length + 1];
+            Array.CopyTo(New, 0);
+            New[Array.Length] = Value;
+            return New;
         }
 
     }
