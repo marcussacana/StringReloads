55eff95435de162339d195d1847f9de315208af2
Marcus André <ddtank.marcus@gmail.com>
2020-04-13 21:26:47 -0300

    Fix LSTEntry Empty Parser

--- StringReloads/Engine/LSTParser.cs
+++ StringReloads/Engine/LSTParser.cs
@@ -69,78 +69,92 @@ namespace StringReloads.Engine
     }
 
 
-        public struct LSTEntry
+    public struct LSTEntry
+    {
+        public LSTEntry(string LineAB) : this(LineAB, LineAB) { }
+        public LSTEntry(string LineA, string LineB, LSTFlag[] FlagsA, LSTFlag[] FlagsB)
         {
-            public LSTEntry(string LineAB) : this(LineAB, LineAB) { }
-            public LSTEntry(string LineA, string LineB, LSTFlag[] FlagsA, LSTFlag[] FlagsB)
+            OriginalLine = LineA;
+            TranslationLine = LineB;
+            OriginalFlags = FlagsA;
+            TranslationFlags = FlagsB;
+        }
+        public LSTEntry(string LineA, string LineB)
+        {
+            if (string.IsNullOrEmpty(LineA) || string.IsNullOrEmpty(LineB))
             {
-                OriginalLine = LineA;
-                TranslationLine = LineB;
-                OriginalFlags = FlagsA;
-                TranslationFlags = FlagsB;
-            }
-            public LSTEntry(string LineA, string LineB)
-            {
-                OriginalFlags = new LSTFlag[0];
-                while (LineA.StartsWith("::"))
-                {
-                    string FlagContent = LineA.Substring(2, LineA.IndexOf("::", 2) - 2);
-                    string Name = FlagContent.Split('-').First();
-                    string Value = null;
-
-                    if (FlagContent.Contains("-"))
-                        Value = FlagContent.Substring(FlagContent.IndexOf("-") + 1);
-
-                    OriginalFlags = OriginalFlags.Union(new LSTFlag[] { new LSTFlag() { Name = Name, Value = Value } }).ToArray();
-
-                    LineA = LineA.Substring(LineA.IndexOf("::", 2) + 2);
-                }
-
+                OriginalLine = string.Empty;
+                TranslationLine = string.Empty;
                 TranslationFlags = new LSTFlag[0];
-                while (LineB.StartsWith("::"))
-                {
-                    string FlagContent = LineB.Substring(2, LineB.IndexOf("::", 2) - 2);
-                    string Name = FlagContent.Split('-').First().Trim();
-                    string Value = null;
+                OriginalFlags = new LSTFlag[0];
+                return;
+            }
+            OriginalFlags = new LSTFlag[0];
+            while (LineA.StartsWith("::"))
+            {
+                string FlagContent = LineA.Substring(2, LineA.IndexOf("::", 2) - 2);
+                string Name = FlagContent.Split('-').First();
+                string Value = null;
 
-                    if (FlagContent.Contains("-"))
-                        Value = FlagContent.Substring(FlagContent.IndexOf("-") + 1).Trim();
+                if (FlagContent.Contains("-"))
+                    Value = FlagContent.Substring(FlagContent.IndexOf("-") + 1);
 
-                    TranslationFlags = TranslationFlags.Union(new LSTFlag[] { new LSTFlag() { Name = Name, Value = Value } }).ToArray();
+                OriginalFlags = OriginalFlags.Union(new LSTFlag[] { new LSTFlag() { Name = Name, Value = Value } }).ToArray();
 
-                    LineB = LineB.Substring(LineB.IndexOf("::", 2) + 2);
-                }
-
-                OriginalLine = LineA;
-                TranslationLine = LineB;
+                LineA = LineA.Substring(LineA.IndexOf("::", 2) + 2);
             }
 
-            public LSTFlag[] Flags => OriginalFlags.Union(TranslationFlags).ToArray();
-            public LSTFlag[] OriginalFlags;
-            public LSTFlag[] TranslationFlags;
-            public string OriginalLine;
-            public string TranslationLine;
+            TranslationFlags = new LSTFlag[0];
+            while (LineB.StartsWith("::"))
+            {
+                string FlagContent = LineB.Substring(2, LineB.IndexOf("::", 2) - 2);
+                string Name = FlagContent.Split('-').First().Trim();
+                string Value = null;
+
+                if (FlagContent.Contains("-"))
+                    Value = FlagContent.Substring(FlagContent.IndexOf("-") + 1).Trim();
+
+                TranslationFlags = TranslationFlags.Union(new LSTFlag[] { new LSTFlag() { Name = Name, Value = Value } }).ToArray();
+
+                LineB = LineB.Substring(LineB.IndexOf("::", 2) + 2);
+            }
+
+            OriginalLine = LineA;
+            TranslationLine = LineB;
         }
 
-        public struct LSTFlag
+        public LSTFlag[] Flags => OriginalFlags.Union(TranslationFlags).ToArray();
+        public LSTFlag[] OriginalFlags;
+        public LSTFlag[] TranslationFlags;
+        public string OriginalLine;
+        public string TranslationLine;
+    }
+
+    public struct LSTFlag
+    {
+        public string Name;
+        public string Value;
+    }
+
+    internal static partial class Extensions
+    {
+        internal static bool HasFlag(this LSTFlag[] Flags, string Name)
         {
-            public string Name;
-            public string Value;
-        }
-
-        internal static partial class Extensions {
-        internal static bool HasFlag(this LSTFlag[] Flags, string Name) {
-            foreach (var Flag in Flags) {
+            foreach (var Flag in Flags)
+            {
                 if (Flag.Name.ToLowerInvariant() == Name.ToLowerInvariant())
                     return true;
             }
             return false;
         }
 
-        internal static string GetFlags(this LSTFlag[] Flags) {
+        internal static string GetFlags(this LSTFlag[] Flags)
+        {
             string Buffer = string.Empty;
-            foreach (var Flag in Flags) {
-                if (Flag.Value != null) {
+            foreach (var Flag in Flags)
+            {
+                if (Flag.Value != null)
+                {
                     Buffer += $"::{Flag.Name}-{Flag.Value}::";
                     continue;
                 }
